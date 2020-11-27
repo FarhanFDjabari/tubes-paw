189ae09a516752d308d170f4e7bcb04360073a17
FarhanFDjabari <ffadhilah8@gmail.com>
2020-11-27 17:56:23 +0700

    change base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/projekakhir/';
+$config['base_url'] = 'http://localhost/tubes-paw/';
 
 /*
 |--------------------------------------------------------------------------
